5c0a0db5d6262026371209d6ba41c0c319ed5ff2
sam-bee <130986804+sam-bee@users.noreply.github.com>
2026-06-13 19:03:10 +0100

    Experiment with transformer state encoder

--- internal/model/policy.go
+++ internal/model/policy.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gomlx/gomlx/pkg/ml/context"
 	"github.com/gomlx/gomlx/pkg/ml/layers"
 	"github.com/gomlx/gomlx/pkg/ml/layers/activations"
+	"github.com/gomlx/gomlx/pkg/ml/layers/attention"
 
 	"github.com/sam-bee/wordle-ml_backprop/internal/data"
 )
@@ -25,6 +26,13 @@ const (
 	DenseTrunkHidden0  = 256
 	DenseTrunkHidden1  = 128
 	PolicyVectorDim    = 64
+
+	TransformerTokenCount       = 1 + data.MaxTurns
+	TransformerModelDim         = 64
+	TransformerHeadCount        = 4
+	TransformerHeadDim          = 16
+	TransformerFeedForwardDim   = 128
+	TransformerPolicyHeadHidden = 128
 
 	FixedActionFeatureDim     = 26
 	TrainableActionFeatureDim = PolicyVectorDim - FixedActionFeatureDim
@@ -61,16 +69,62 @@ func PolicyModel(ctx *context.Context, _ any, inputs []*graph.Node) []*graph.Nod
 func PolicyVector(ctx *context.Context, turnFeatures, occupiedTurns, virginGrid *graph.Node) *graph.Node {
 	validateDecisionStateInputs(turnFeatures, occupiedTurns, virginGrid)
 
-	encodedTurns := EncodeTurns(ctx.In("input_encoder"), turnFeatures, occupiedTurns)
-	batchSize := encodedTurns.Shape().Dim(0)
+	stateToken := TransformerStateToken(ctx.In("state_transformer"), turnFeatures, occupiedTurns)
+	modelInput := graph.Concatenate([]*graph.Node{virginGrid, stateToken}, -1)
 
-	flatTurns := graph.Reshape(encodedTurns, batchSize, data.MaxTurns*EncodedTurnFeatureDim)
-	modelInput := graph.Concatenate([]*graph.Node{virginGrid, flatTurns}, -1)
+	head := ctx.In("policy_head")
+	hidden := activations.Relu(dense(head.In("input_to_hidden"), modelInput, TransformerPolicyHeadHidden))
+	return dense(head.In("hidden_to_output"), hidden, PolicyVectorDim)
+}
 
-	trunk := ctx.In("dense_trunk")
-	hidden0 := activations.Relu(dense(trunk.In("input_to_hidden0"), modelInput, DenseTrunkHidden0))
-	hidden1 := activations.Relu(dense(trunk.In("hidden0_to_hidden1"), hidden0, DenseTrunkHidden1))
-	return dense(trunk.In("hidden1_to_output"), hidden1, PolicyVectorDim)
+// TransformerStateToken maps a Wordle decision state to a single contextual state vector.
+func TransformerStateToken(ctx *context.Context, turnFeatures, occupiedTurns *graph.Node) *graph.Node {
+	validateTurnEncoderInputs(turnFeatures, occupiedTurns)
+
+	batchSize := turnFeatures.Shape().Dim(0)
+	g := turnFeatures.Graph()
+
+	flatTurns := graph.Reshape(turnFeatures, batchSize*data.MaxTurns, RawTurnFeatureCount)
+	turnTokens := dense(ctx.In("turn_projection"), flatTurns, TransformerModelDim)
+	turnTokens = graph.Reshape(turnTokens, batchSize, data.MaxTurns, TransformerModelDim)
+
+	stateToken := ctx.WithInitializer(outputTailInitializer(ctx)).
+		VariableWithShape("state_token", shapes.Make(turnFeatures.DType(), 1, 1, TransformerModelDim)).
+		ValueGraph(g)
+	stateToken = graph.BroadcastToDims(stateToken, batchSize, 1, TransformerModelDim)
+
+	tokens := graph.Concatenate([]*graph.Node{stateToken, turnTokens}, 1)
+	positionEmbeddings := ctx.WithInitializer(outputTailInitializer(ctx)).
+		VariableWithShape("position_embeddings", shapes.Make(turnFeatures.DType(), 1, TransformerTokenCount, TransformerModelDim)).
+		ValueGraph(g)
+	positionEmbeddings = graph.BroadcastToDims(positionEmbeddings, batchSize, TransformerTokenCount, TransformerModelDim)
+	tokens = graph.Add(tokens, positionEmbeddings)
+
+	keyMask := transformerKeyMask(g, occupiedTurns, batchSize)
+	tokens = transformerBlock(ctx.In("block0"), tokens, keyMask)
+
+	state := graph.Slice(tokens, graph.AxisRange(), graph.AxisElem(0), graph.AxisRange())
+	return graph.Reshape(state, batchSize, TransformerModelDim)
+}
+
+func transformerBlock(ctx *context.Context, tokens, keyMask *graph.Node) *graph.Node {
+	attnInput := layers.LayerNormalization(ctx.In("attn_norm"), tokens, -1).Done()
+	attnOutput, _ := attention.SelfAttention(ctx.In("self_attention"), attnInput, TransformerHeadCount, TransformerHeadDim).
+		WithKeyMask(keyMask).
+		WithOutputDim(TransformerModelDim).
+		DoneWithCoefficients()
+	tokens = graph.Add(tokens, attnOutput)
+
+	ffnInput := layers.LayerNormalization(ctx.In("ffn_norm"), tokens, -1).Done()
+	ffnHidden := activations.Relu(dense(ctx.In("ffn").In("input_to_hidden"), ffnInput, TransformerFeedForwardDim))
+	ffnOutput := dense(ctx.In("ffn").In("hidden_to_output"), ffnHidden, TransformerModelDim)
+	return graph.Add(tokens, ffnOutput)
+}
+
+func transformerKeyMask(g *graph.Graph, occupiedTurns *graph.Node, batchSize int) *graph.Node {
+	stateMask := graph.BroadcastToDims(graph.Const(g, true), batchSize, 1)
+	occupiedMask := graph.GreaterThan(occupiedTurns, graph.ConstAsDType(g, occupiedTurns.DType(), 0.5))
+	return graph.Concatenate([]*graph.Node{stateMask, occupiedMask}, -1)
 }
 
 // EncodeTurns applies the shared per-turn encoder and zeroes unoccupied slots.
